67b0717c44699b279970635a658df9c410fccd3a
o0kazuya0o <o0kazuya0o@ezweb.ne.jp>
2019-07-06 13:18:13 +0900

    Revert "Playerクラス作成"
    This reverts commit 33b486accf4257874ef60bb56fb2637b4c6c8926.

--- PlayerObject.cpp
+++ PlayerObject.cpp
@@ -1,21 +1,12 @@
 #include "PlayerObject.h"
-#include "MeshComponent.h"
-#include "Game.h"
-#include "Renderer.h"
-#include "FollowCamera.h"
-#include "MoveComponent.h"
-#include "SphereCollider.h"
+
 
 PlayerObject::PlayerObject(Game* game)
 	:GameObject(game)
 {
 }
 
+
 PlayerObject::~PlayerObject()
 {
-}
-
-void PlayerObject::SetVisible(bool visible)
-{
-    meshComp->SetVisible(visible);
 }
--- PlayerObject.h
+++ PlayerObject.h
@@ -1,24 +1,10 @@
 #pragma once
 #include "GameObject.h"
-
-class MoveComponent;
-class FollowCamera;
-class MeshComponent;
-class SphereCollider;
-
-class PlayerObject : public GameObject
+class PlayerObject :
+	public GameObject
 {
 public:
 	PlayerObject(Game* game);
 	~PlayerObject();
-
-    void SetVisible(bool visible);
-
-private:
-
-    MoveComponent* moveComp;
-    FollowCamera* cameraComp;
-    MeshComponent* meshComp;
-    SphereCollider* sphereCollider;
 };
 
