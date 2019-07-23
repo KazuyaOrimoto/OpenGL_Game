84cf0afc3037d9e64df0a96150d7bce985d31643
kazuya <o0kazuya0o@gmail.com>
2019-07-23 15:21:43 +0900

    障害物の表示

--- BoxObject.cpp
+++ BoxObject.cpp
@@ -9,11 +9,11 @@ BoxObject::BoxObject(Game* game)
     :GameObject(game)
 {
     meshComp = new MeshComponent(this);
-    meshComp->SetMesh(RENDERER->GetMesh("Assets/Cube.gpmesh"));
+    meshComp->SetMesh(RENDERER->GetMesh("Assets/Obstacle.gpmesh"));
     boxCollider = new BoxCollider(this);
     boxCollider->SetObjectBox(AABB(Vector3(-0.5f, -0.5f, -0.5f), Vector3(0.5f, 0.5f, 0.5f)));
-    SetPosition(Vector3(2000.0f, -75.0f, 150.0f));
-    SetScale(100.0f);
+    SetPosition(Vector3(2000.0f, 900.0f, 0.0f));
+    SetScale(250.0f);
 }
 
 
--- GameObjectManager.h
+++ GameObjectManager.h
@@ -1,4 +1,4 @@
-#pragma once
+                                                                                      #pragma once
 
 #define GAME_OBJECT_MANAGER GameObjectManager::GetInstance()
 
