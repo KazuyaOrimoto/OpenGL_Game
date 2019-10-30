4edcd5728ae2efbac0a2fa4f37724b6fb0122ed4
o0kazuya0o <o0kazuya0o@ezweb.ne.jp>
2019-10-30 16:18:31 +0900

    障害物を見えやすくするためにライトの方向を変更

--- PlayScene.cpp
+++ PlayScene.cpp
@@ -10,8 +10,8 @@ PlayScene::PlayScene()
     // Setup lights
     RENDERER->SetAmbientLight(Vector3(0.4f, 0.4f, 0.4f));
     DirectionalLight& dir = RENDERER->GetDirectionalLight();
-    dir.direction = Vector3(1.0f, 0.7f, -0.7f);
-    dir.diffuseColor = Vector3(0.78f, 0.88f, 1.0f);
+    dir.direction = Vector3(-1.0f, 0.7f, 0.7f);
+    dir.diffuseColor = Vector3(0.9f, 0.98f, 1.0f);
     dir.specColor = Vector3(0.8f, 0.8f, 0.8f);
 
     GAME_OBJECT_CREATER->PlayerAndWallCreate();
